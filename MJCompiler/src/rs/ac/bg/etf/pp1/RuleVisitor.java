92cb6393f28c57df44d0e736fb5294cfe6def62a
tzomyy <106305602+tzomyy@users.noreply.github.com>
2023-01-30 17:54:54 +0100

    Added new classes, RuleVisitor and MJParserTest
    Moram da definisem program u .cup fajlu da bih testirala

---
+++ MJCompiler/src/rs/ac/bg/etf/pp1/RuleVisitor.java
@@ -0,0 +1,14 @@
+package rs.ac.bg.etf.pp1;
+
+import org.apache.log4j.Logger;
+import rs.ac.bg.etf.pp1.ast.*;
+
+public class RuleVisitor extends VisitorAdaptor{
+
+	int printCallCount = 0;
+	int varDeclCount = 0;
+	
+	Logger log = Logger.getLogger(getClass());
+
+}
+
